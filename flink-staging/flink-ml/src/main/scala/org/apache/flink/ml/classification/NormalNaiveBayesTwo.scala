710d514585029a8bcbf769dd76ba3408cfb5f119
Jonathan Hasenburg <jonathan.hasenburg@campus.tu-berlin.de>
2015-05-28 16:29:35 +0200

    Classifier works

--- flink-staging/flink-ml/src/main/scala/org/apache/flink/ml/classification/NormalNaiveBayesTwo.scala
+++ flink-staging/flink-ml/src/main/scala/org/apache/flink/ml/classification/NormalNaiveBayesTwo.scala
@@ -1,7 +1,10 @@
 package org.apache.flink.ml.classification
 
-import org.apache.flink.api.common.functions.{RichMapFunction, FlatMapFunction}
+import java.lang.Iterable
+
+import org.apache.flink.api.common.functions.{ReduceFunction, RichMapPartitionFunction, FlatMapFunction}
 import org.apache.flink.api.scala._
+import org.apache.flink.configuration.Configuration
 import org.apache.flink.core.fs.FileSystem.WriteMode
 import org.apache.flink.ml.common._
 import org.apache.flink.util.Collector
@@ -11,9 +14,9 @@ import scala.collection.mutable.Map
 /**
  * Created by jonathanhasenburg on 05.05.15.
  */
-class NormalNaiveBayesTwo extends Learner[(String, String), NormalNaiveBayesModelTwoOneString] with Serializable {
+class NormalNaiveBayesTwo extends Learner[(String, String), NormalNaiveBayesModelTwo] with Serializable {
 
- override def fit(input: DataSet[(String, String)], fitParameters: ParameterMap): NormalNaiveBayesModelTwoOneString = {
+ override def fit(input: DataSet[(String, String)], fitParameters: ParameterMap): NormalNaiveBayesModelTwo = {
 
    // Classname -> Count of documents
    val documentsPerClass: DataSet[(String, Int)] = input.map { line => (line._1, 1) }
@@ -37,13 +40,16 @@ class NormalNaiveBayesTwo extends Learner[(String, String), NormalNaiveBayesMode
      (single, all) => (single._1, single._2, single._3, all._2)
    }
 
-
    // Count of all documents
+   //val documentCount: Double = documentsPerClass.reduce((line1, line2) => (line1._1, line1._2 + line1._2)) // TODO -> aus documentsPerClass -> reduce and add
    val documentsCount: Double= input.count()
 
    // All words, but distinct
    val vocabulary = singleWordsInClass.map(tuple => (tuple._2, 1)).distinct(0)
    val vocabularyCount: Double = vocabulary.count
+
+   println("Document count = " + documentsCount)
+   println("Vocabulary count = " + vocabularyCount)
 
    //******************************************************************************************************************
    //calculate P(w) and P(w|c)
@@ -76,7 +82,7 @@ class NormalNaiveBayesTwo extends Learner[(String, String), NormalNaiveBayesMode
   //val outputs : DataSet[classPoJo] = probabilityDataSet.map(line => )
    probabilityDataSet.writeAsText("/Users/jonathanhasenburg/Desktop/naiveB/probabilityDataSet.txt", WriteMode.OVERWRITE)
 
-   return new NormalNaiveBayesModelTwoOneString(probabilityDataSet)
+   return new NormalNaiveBayesModelTwo(probabilityDataSet)
 
   }
 
@@ -101,7 +107,7 @@ class NormalNaiveBayesTwo extends Learner[(String, String), NormalNaiveBayesMode
 
   /**
    * Transforms a (String, String) tuple into a (String, String, Int)) tuple.
-   * The second string from the input gets split into it words, for each word a tuple is collected with the Int (count) 1
+   * The second string from the input gets split into its words, for each word a tuple is collected with the Int (count) 1
    */
   class SingleWordSplitter() extends FlatMapFunction[(String, String), (String, String, Int)] {
     override def flatMap(value: (String, String), out: Collector[(String, String, Int)]): Unit = {
@@ -109,92 +115,115 @@ class NormalNaiveBayesTwo extends Learner[(String, String), NormalNaiveBayesMode
         out.collect((value._1, token, 1))
       }
     }
+
+    // Can be implemented via: input.flatMap{ pair => pair._2.split(" ").map{ token => (pair._1, token ,1)} }
   }
+
+}
+
+/**
+ *
+ * @param probabilityDataSet  Classname -> Word -> log(P(w|c)) -> log(P(c)) -> log(p(w|c)) not in class
+ */
+class NormalNaiveBayesModelTwo(probabilityDataSet: DataSet[(String, String, Double, Double, Double)]) extends Transformer[(Int, String), (Int, String)] {
+
 
   /**
-   * Creates a Dataset with a 1 for each document by submitting a 1 value._2 amount of times for each value._1
+   * Identifies the classes of an Input DataSet[ID -> Text] by creating an Output DataSet[ID -> Class]
+   * @param input
+   * @param transformParameters
+   * @return
    */
-  class DocumentCounterByCreatingEntries() extends FlatMapFunction[(String, Int), Int] {
-    override def flatMap(value: (String, Int), out: Collector[Int]): Unit = {
-      for (i <- 1 to value._2) {
-        out.collect(1)
-      }
+  override def transform(input: DataSet[(Int, String)], transformParameters: ParameterMap): DataSet[(Int, String)] = {
+
+    // Splits the texts from the input DataSet into its words
+    val words : DataSet[(Int, String)] = input.flatMap {
+      pair => pair._2.split(" ").map{ word => (pair._1, word)}
     }
-  }
 
-}
+    // genreate word counts for each word with a key
+    // 1. Map: put a 1 to each key
+    // 2. Group-Reduce: group by id and word and sum the 1s
+    val wordsAndCount: DataSet[(Int, String, Int)] = words.map(line => (line._1, line._2, 1)).groupBy(0, 1).sum(2) // id -> word -> wordcount in text
+
+    //calculate the count of all words for a text identified by its key
+    val wordsInText: DataSet[(Int, Int)] = wordsAndCount.map(line => (line._1, line._3)).groupBy(0).sum(1) //id -> all words in text
 
 
-class NormalNaiveBayesModelTwo(probabilityDataSet: DataSet[(String, String, Double, Double, Double)]) extends Transformer[String, (String, String)] {
-
-  override def transform(input: DataSet[String], transformParameters: ParameterMap): DataSet[(String, String)] = {
-
-
-
-    return null
-  }
-}
-
-class NormalNaiveBayesModelTwoOneString(probabilityDataSet: DataSet[(String, String, Double, Double, Double)]) extends Transformer[String, (String, String)] {
-  override def transform(input: DataSet[String], transformParameters: ParameterMap): DataSet[(String, String)] = {
-
-    val words : DataSet[(String)] = input.flatMap { _.split(" ") }
-
-    val wordsInInput = words.count()
-
-    //split input text in words
-    val wordsAndCount: DataSet[(String, Int)] = words.map{line => (line, 1)}.groupBy(0).sum(1)
-
-    //join probabilityDataSet and words to classname -> Word -> wordcount in document -> log(P(w|c)) -> log(P(c)) -> log(p(w|c)) not in class
-    val joinedWords = wordsAndCount.join(probabilityDataSet).where(0).equalTo(1) {
-      (words, probabilityDataSet) => (probabilityDataSet._1, words._1, words._2, probabilityDataSet._3, probabilityDataSet._4, probabilityDataSet._5)
-    }
+    //join probabilityDataSet and words
+    val joinedWords = wordsAndCount.join(probabilityDataSet).where(1).equalTo(1) {
+      (words, probabilityDataSet) => (words._1, probabilityDataSet._1, words._2, words._3, probabilityDataSet._3, probabilityDataSet._4, probabilityDataSet._5)
+    } // id -> classname -> Word -> wordcount in text (each word) -> log(P(w|c)) -> log(P(c)) -> log(p(w|c)) not in class
 
     joinedWords.writeAsText("/Users/jonathanhasenburg/Desktop/naiveB/joinedWords.txt", WriteMode.OVERWRITE)
 
+
     //calculate sumpwc for found words
-      // 1. Map: only needed information: class -> wordcount in document (each word) *  log(p(w|c)) each word
-      // 2. Group-Reduce: sum log(p(w|c))
-    val sumPwcFoundWords : DataSet[(String, Double)] = joinedWords.map(line => (line._1, line._3 * line._4))
-      .groupBy(0)
-      .reduce((line1, line2) => (line1._1, line1._2 + line2._2)) // class -> sum log(p(w|c)) for all found words
+    // 1. Map: only needed information: id -> class -> wordcount in text (each word) *  log(p(w|c)) each word
+    // 2. Group-Reduce: sum log(p(w|c))
+    val sumPwcFoundWords: DataSet[(Int, String, Double)] = joinedWords.map(line => (line._1, line._2 ,line._4 * line._5))
+      .groupBy(0, 1)
+      .reduce((line1, line2) => (line1._1, line1._2, line1._3 + line2._3)) // id -> class -> sum log(p(w|c)) for all found words in class
 
     sumPwcFoundWords.writeAsText("/Users/jonathanhasenburg/Desktop/naiveB/sumPwcFoundWords.txt", WriteMode.OVERWRITE)
 
+
     //calculate the amount of found words for each class
-      // 1. Map: only needed information: class -> wordcount in document (each word) -> log(p(w|c)) not in class
-      // 2. Group-Reduce: sum of wordcounts in document
-      // 3. Map: calculate sumPwcNotFound: class -> (words.count -  sumwordcount) * log(p(w|c)) not in class
-    val sumPwcNotFoundWords: DataSet[(String, Double)] = joinedWords.map(line => (line._1, line._3, line._6))
-      .groupBy(0)
-      .reduce((line1, line2) => (line1._1, line1._2 + line2._2, line1._3))
-      .map(line => (line._1, (wordsInInput - line._2) * line._3))// class -> sum log(p(w|c)) for all not found words
+    // 1. Map: only needed information: id -> class -> wordcount in text (each word) -> log(p(w|c)) not in class
+    // 2. Group-Reduce: sum of word counts in text
+    // 3. Join with wordsInText to get the count of all words per text
+    // 4. Map: calculate sumPwcNotFound: id -> class -> (word counts in text -  wordsInText) * log(p(w|c)) not in class
+    val sumPwcNotFoundWords: DataSet[(Int, String, Double)] = joinedWords.map(line => (line._1, line._2, line._4, line._7))
+      .groupBy(0, 1)
+      .reduce((line1, line2) => (line1._1, line1._2, line1._3 + line2._3, line1._4))
+      .join(wordsInText).where(0).equalTo(0) {
+        (line1, line2) => (line1._1, line1._2, line1._3, line1._4, line2._2)
+      }.map(line => (line._1, line._2, (line._5 - line._3) * line._4)) // id -> class -> sum log(p(w|c)) for all not found words
 
     sumPwcNotFoundWords.writeAsText("/Users/jonathanhasenburg/Desktop/naiveB/sumPwcNotFoundWords.txt", WriteMode.OVERWRITE)
 
     //sum those pwc sums
-      // 1. Join sumPwcFoundWords and sumPwcNotFoundWords
-      // 2. Map: add sums from found words and sums from not found words
-    val sumPwc: DataSet[(String, Double)] = sumPwcFoundWords.join(sumPwcNotFoundWords).where(0).equalTo(0) {
-      (found, notfound) => (found._1, found._2 + notfound._2)
-    }
+    // 1. Join sumPwcFoundWords and sumPwcNotFoundWords
+    // 2. Map: add sums from found words and sums from not found words
+    val sumPwc: DataSet[(Int, String, Double)] = sumPwcFoundWords.join(sumPwcNotFoundWords).where(0, 1).equalTo(0, 1) {
+      (found, notfound) => (found._1, found._2, found._3 + notfound._3)
+    } //id -> classname -> sum log(p(w|c))
 
     sumPwc.writeAsText("/Users/jonathanhasenburg/Desktop/naiveB/sumPwc.txt", WriteMode.OVERWRITE)
 
+
     //calculate possibility for each class
-      // 1. only needed information: class -> log(p(c))
-      // 2. Group-Reduce probabilitySet to one entry for each class
-      // 3. Join sumPwc with probability DataSet to get log(P(c))
-      // 4. Map: add sumPwc with log(P(c))
-    val possibility: DataSet[(String, Double)] = probabilityDataSet.map(line => (line._1, line._4))
+    // 1. only needed information: class -> log(p(c))
+    // 2. Group-Reduce probabilitySet to one entry for each class
+    // 3. Join sumPwc with probability DataSet to get log(P(c))
+    // 4. Map: add sumPwc with log(P(c))
+    val possibility: DataSet[(Int, String, Double)] = probabilityDataSet.map(line => (line._1, line._4))
       .groupBy(0)
       .reduce((line1, line2) => (line1._1, line1._2))
-      .join(sumPwc).where(0).equalTo(0) {
-      (prob, pwc) => (prob._1, prob._2 + pwc._2)
+      .join(sumPwc).where(0).equalTo(1) {
+        (prob, pwc) => (pwc._1, prob._1, prob._2 + pwc._3)
       }
 
     possibility.writeAsText("/Users/jonathanhasenburg/Desktop/naiveB/possibility.txt", WriteMode.OVERWRITE)
 
-    return null
+    val calculatedClasses: DataSet[(Int, String)] = possibility.groupBy(0).reduce(new CalculateReducer()).map(line => (line._1, line._2))
+
+    return calculatedClasses
+
+  }
+
+  def saveModelDataSet(location: String) : Unit = {
+    probabilityDataSet.writeAsCsv(location, "\n", "|", WriteMode.OVERWRITE)
+  }
+
+}
+
+class CalculateReducer() extends ReduceFunction[(Int, String, Double)] {
+  override def reduce(value1: (Int, String, Double), value2: (Int, String, Double)): (Int, String, Double) = {
+    if (value1._3 > value2._3) {
+      return value1;
+    } else {
+      return value2;
+    }
   }
 }
--- flink-staging/flink-ml/src/test/scala/org/apache/flink/ml/classification/NormalNaiveBayesITSuite.scala
+++ flink-staging/flink-ml/src/test/scala/org/apache/flink/ml/classification/NormalNaiveBayesITSuite.scala
@@ -18,11 +18,15 @@
 
 package org.apache.flink.ml.classification
 
+import org.apache.flink.api.java.DataSet
 import org.apache.flink.api.scala._
+import org.apache.flink.core.fs.FileSystem.WriteMode
 import org.apache.flink.test.util.FlinkTestBase
 import org.scalatest.{FlatSpec, Matchers}
 
 class NormalNaiveBayesITSuite extends FlatSpec with Matchers with FlinkTestBase {
+
+  val saveLocationModel = "/Users/jonathanhasenburg/Desktop/naiveB/model.csv"
 
   behavior of "The NormalNaiveBayes implementation"
 
@@ -31,12 +35,17 @@ class NormalNaiveBayesITSuite extends FlatSpec with Matchers with FlinkTestBase 
     env.setParallelism(1)
 
     val learner = new NormalNaiveBayesTwo()
-    val trainingDS = env.readCsvFile[(String, String)]("/Users/jonathanhasenburg/Desktop/naiveB/testfile2.txt", "\n", "\t")
+    //val trainingDS = env.readCsvFile[(String, String)]("/Users/jonathanhasenburg/Desktop/naiveB/testfile2.txt", "\n", "\t")
+    val trainingDS = env.readCsvFile[(String, String)]("/Users/jonathanhasenburg/Desktop/bayes/bbcTrain.csv", "\n", "\t")
+
 
     val model = learner.fit(trainingDS)
 
+    model.saveModelDataSet(saveLocationModel)
+
+
     //model.transform(env.fromElements("and this is god belief belief it or not correct is what you think tragic tragedy is not what god want"))
-    model.transform(env.fromElements("christian moral articl eastman dp nasa kodak write articl vice ico tek bobb vice ico tek robert beauchain write not believ god not concern disposit beneath provid evid requir evid that person thi god find compel fact god you you for minut you love you you made love you want love you don expect love don even exist secondli wouldn expect love simpli creator expect earn that love respons you love god and step promis for you you for you daft love don exist come back you learn love your testicl doubt thi disput not givin sincer effort simpl logic argument folli you read bibl you will that jesu made fool trick logic abil reason spec creation ultim you reli simpli your reason you will never you learn you accept that you don point you step line and becom complet asshol even your offens won slip becuas heard goddamn time you love jesu deep your heart you cannibalist necrophiliac and qualifi assess your motiv you fortun thing accept evid faith that christian quit fuck arrog will never peac you made that bob beauchain bobb vice ico tek that queen stai blew bronx and sank manhattan sea"))
+   // model.transform(env.fromElements((1, "christian moral articl eastman dp nasa kodak write articl vice ico tek bobb vice ico tek robert beauchain write not believ god not concern disposit beneath provid evid requir evid that person thi god find compel fact god you you for minut you love you you made love you want love you don expect love don even exist secondli wouldn expect love simpli creator expect earn that love respons you love god and step promis for you you for you daft love don exist come back you learn love your testicl doubt thi disput not givin sincer effort simpl logic argument folli you read bibl you will that jesu made fool trick logic abil reason spec creation ultim you reli simpli your reason you will never you learn you accept that you don point you step line and becom complet asshol even your offens won slip becuas heard goddamn time you love jesu deep your heart you cannibalist necrophiliac and qualifi assess your motiv you fortun thing accept evid faith that christian quit fuck arrog will never peac you made that bob beauchain bobb vice ico tek that queen stai blew bronx and sank manhattan sea")))
 
     env.execute()
 
@@ -50,5 +59,26 @@ class NormalNaiveBayesITSuite extends FlatSpec with Matchers with FlinkTestBase 
         weight should be(expectedWeight +- 0.1)
     }
     */
+  }
+
+  it should "transform an input string" in {
+
+    val env = ExecutionEnvironment.getExecutionEnvironment
+    env.setParallelism(1)
+
+    val modelSet = env.readCsvFile[(String, String, Double, Double, Double)](saveLocationModel, "\n", "|")
+
+    val model = new NormalNaiveBayesModelTwo(modelSet)
+
+    //val solution = model.transform(env.fromElements((1, "christian moral articl eastman dp nasa kodak write articl vice ico tek bobb vice ico tek robert beauchain write not believ god not concern disposit beneath provid evid requir evid that person thi god find compel fact god you you for minut you love you you made love you want love you don expect love don even exist secondli wouldn expect love simpli creator expect earn that love respons you love god and step promis for you you for you daft love don exist come back you learn love your testicl doubt thi disput not givin sincer effort simpl logic argument folli you read bibl you will that jesu made fool trick logic abil reason spec creation ultim you reli simpli your reason you will never you learn you accept that you don point you step line and becom complet asshol even your offens won slip becuas heard goddamn time you love jesu deep your heart you cannibalist necrophiliac and qualifi assess your motiv you fortun thing accept evid faith that christian quit fuck arrog will never peac you made that bob beauchain bobb vice ico tek that queen stai blew bronx and sank manhattan sea")))
+
+    //val solution = model.transform(env.readCsvFile[(Int, String)]("/Users/jonathanhasenburg/Desktop/naiveB/testfile3.txt", "\n", "\t"))
+
+    val solution = model.transform(env.readCsvFile[(Int, String)]("/Users/jonathanhasenburg/Desktop/bayes/bbcTest.csv", "\n", "\t"))
+
+    solution.writeAsCsv("/Users/jonathanhasenburg/Desktop/bayes/bbcTestComputedCategories.csv", "\n", "\t", WriteMode.OVERWRITE)
+
+    env.execute()
+
   }
 }
