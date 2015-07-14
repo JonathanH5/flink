f62df64e972dcc904d18db2673efbe1c1a8141ee
Jonathan Hasenburg <jonathan.hasenburg@campus.tu-berlin.de>
2015-07-14 11:35:57 +0200

    NaiveB now working with new MLL Layout

--- flink-staging/flink-ml/src/main/scala/org/apache/flink/ml/classification/NormalNaiveBayes.scala
+++ flink-staging/flink-ml/src/main/scala/org/apache/flink/ml/classification/NormalNaiveBayes.scala
@@ -8,7 +8,7 @@ import org.apache.flink.api.scala._
 import org.apache.flink.configuration.Configuration
 import org.apache.flink.core.fs.FileSystem.WriteMode
 import org.apache.flink.ml.common._
-import org.apache.flink.ml.pipeline.{PredictOperation, FitOperation, Predictor}
+import org.apache.flink.ml.pipeline.{PredictDataSetOperation, PredictOperation, FitOperation, Predictor}
 import org.apache.flink.util.Collector
 import scala.collection.mutable.Map
 
@@ -17,6 +17,14 @@ class NormalNaiveBayes extends Predictor[NormalNaiveBayes] {
 
   //The model, that stores all needed information after the fitting phase
   var probabilityDataSet: Option[DataSet[(String, String, Double, Double, Double)]] = None
+
+  def saveModelDataSet(location: String) : Unit = {
+    probabilityDataSet.get.writeAsCsv(location, "\n", "|", WriteMode.OVERWRITE)
+  }
+
+  def loadModelDataSet(loaded : DataSet[(String, String, Double, Double, Double)]) : Unit = {
+    this.probabilityDataSet = Some(loaded)
+  }
 
   //Configuration options
 
@@ -36,7 +44,7 @@ class NormalNaiveBayes extends Predictor[NormalNaiveBayes] {
 
 object NormalNaiveBayes {
 
-   // ======================================== Factory methods ======================================
+   // ======================================== Factory Methods =====================================
 
   def apply(): NormalNaiveBayes = {
     new NormalNaiveBayes()
@@ -80,7 +88,7 @@ object NormalNaiveBayes {
 
       // All words, but distinct
       val vocabulary = singleWordsInClass.map(tuple => (tuple._2, 1)).distinct(0)
-      val vocabularyCount: Double = vocabulary.count
+      val vocabularyCount: Double = vocabulary.count()
 
       println("Document count = " + documentsCount)
       println("Vocabulary count = " + vocabularyCount)
@@ -88,9 +96,10 @@ object NormalNaiveBayes {
       //******************************************************************************************************************
       //calculate P(w) and P(w|c)
 
-      // Classname -> P(w)  in class POSSIBILITY 1
+      // Classname -> P(w) in class
+      // TODO POSSIBILITY 1
       val pw: DataSet[(String, Double)] = documentsPerClass.map(line => (line._1, line._2 / documentsCount))
-      pw.print()
+      // TODO Possibility 2
       val pw2: DataSet[(String, Double)] = documentsPerClass.map(new RichMapFunction[(String, Int), (String, Double)] {
 
         var broadcastSet: util.List[Double] = null
@@ -101,11 +110,10 @@ object NormalNaiveBayes {
         }
 
         override def map(value: (String, Int)): (String, Double) = {
-          return (value._1, value._2 / broadcastSet.get(0))
+          (value._1, value._2 / broadcastSet.get(0))
         }
       }).withBroadcastSet(documentsCount2, "documentCount")
 
-      pw2.print()
 
       // Classname -> P(w|c) word not in class
       val pwcNotInClass: DataSet[(String, Double)] = allWordsInClass.map(line => (line._1, 1 / (line._2 + vocabularyCount)))
@@ -128,97 +136,84 @@ object NormalNaiveBayes {
     }
   }
 
-  implicit def predictNNB[inputD : DataSet[(Int, String)] = {
-    new PredictOperation[NormalNaiveBayes, (String, String, Double, Double, Double), inputD, DataSet[(Int, String)]]() {
-      /** Defines how to retrieve the model of the type for which this operation was defined
-        *
-        * @param instance The Predictor instance that we will use to make the predictions
-        * @param predictParameters The parameters for the prediction
-        * @return A DataSet with the model representation as its only element
-        */
-      override def getModel(instance: NormalNaiveBayes, predictParameters: ParameterMap): DataSet[(String, String, Double, Double, Double)] = {
-        self.probabilityDataset match {
-          case Some(weights) => probabilityDataset
+  // Model (String, String, Double, Double, Double)
 
+  implicit def predictNNB = new PredictDataSetOperation[NormalNaiveBayes, (Int, String), (Int, String)]() {
 
-          case None => {
-            throw new RuntimeException("The NormalNaiveBayes has not been fitted to the " +
-              "data. This is necessary before a prediction on other data can be made.")
-          }
-        }
+    override def predictDataSet(instance: NormalNaiveBayes,
+                                predictParameters: ParameterMap,
+                                input: DataSet[(Int, String)]): DataSet[(Int, String)] = {
+
+      if (instance.probabilityDataSet.equals(None)) {
+        throw new RuntimeException("The NormalNaiveBayes has not been fitted to the " +
+            "data. This is necessary before a prediction on other data can be made.")
       }
 
-      /** Calculates the prediction for a single element given the model of the [[Predictor]].
-        *
-        * @param input The input dataset, Int -> String (ID -> Text)
-        * @param probabilityDataSet The model representation based on a fitted train set
-        * @return The output dataset, Int -> String (ID -> Text)
-        */
-      override def predict(input: inputD, probabilityDataSet: DataSet[(String, String, Double, Double, Double)]): DataSet[(Int, String)] = {
+      val probabilityDataSet = instance.probabilityDataSet.get;
 
-        // Splits the texts from the input DataSet into its words
-        val words : DataSet[(Int, String)] = input.flatMap {
-          pair => pair._2.split(" ").map{ word => (pair._1, word)}
-        }
-
-        // genreate word counts for each word with a key
-        // 1. Map: put a 1 to each key
-        // 2. Group-Reduce: group by id and word and sum the 1s
-        val wordsAndCount: DataSet[(Int, String, Int)] = words.map(line => (line._1, line._2, 1)).groupBy(0, 1).sum(2) // id -> word -> wordcount in text
-
-        //calculate the count of all words for a text identified by its key
-        val wordsInText: DataSet[(Int, Int)] = wordsAndCount.map(line => (line._1, line._3)).groupBy(0).sum(1) //id -> all words in text
-
-
-        //join probabilityDataSet and words
-        val joinedWords = wordsAndCount.join(probabilityDataSet).where(1).equalTo(1) {
-          (words, probabilityDataSet) => (words._1, probabilityDataSet._1, words._2, words._3, probabilityDataSet._3, probabilityDataSet._4, probabilityDataSet._5)
-        } // id -> classname -> Word -> wordcount in text (each word) -> log(P(w|c)) -> log(P(c)) -> log(p(w|c)) not in class
-
-        //calculate sumpwc for found words
-        // 1. Map: only needed information: id -> class -> wordcount in text (each word) *  log(p(w|c)) each word
-        // 2. Group-Reduce: sum log(p(w|c))
-        val sumPwcFoundWords: DataSet[(Int, String, Double)] = joinedWords.map(line => (line._1, line._2 ,line._4 * line._5))
-          .groupBy(0, 1)
-          .reduce((line1, line2) => (line1._1, line1._2, line1._3 + line2._3)) // id -> class -> sum log(p(w|c)) for all found words in class
-
-        //calculate the amount of found words for each class
-        // 1. Map: only needed information: id -> class -> wordcount in text (each word) -> log(p(w|c)) not in class
-        // 2. Group-Reduce: sum of word counts in text
-        // 3. Join with wordsInText to get the count of all words per text
-        // 4. Map: calculate sumPwcNotFound: id -> class -> (word counts in text -  wordsInText) * log(p(w|c)) not in class
-        val sumPwcNotFoundWords: DataSet[(Int, String, Double)] = joinedWords.map(line => (line._1, line._2, line._4, line._7))
-          .groupBy(0, 1)
-          .reduce((line1, line2) => (line1._1, line1._2, line1._3 + line2._3, line1._4))
-          .join(wordsInText).where(0).equalTo(0) {
-          (line1, line2) => (line1._1, line1._2, line1._3, line1._4, line2._2)
-        }.map(line => (line._1, line._2, (line._5 - line._3) * line._4)) // id -> class -> sum log(p(w|c)) for all not found words
-
-        //sum those pwc sums
-        // 1. Join sumPwcFoundWords and sumPwcNotFoundWords
-        // 2. Map: add sums from found words and sums from not found words
-        val sumPwc: DataSet[(Int, String, Double)] = sumPwcFoundWords.join(sumPwcNotFoundWords).where(0, 1).equalTo(0, 1) {
-          (found, notfound) => (found._1, found._2, found._3 + notfound._3)
-        } //id -> classname -> sum log(p(w|c))
-
-        //calculate possibility for each class
-        // 1. only needed information: class -> log(p(c))
-        // 2. Group-Reduce probabilitySet to one entry for each class
-        // 3. Join sumPwc with probability DataSet to get log(P(c))
-        // 4. Map: add sumPwc with log(P(c))
-        val possibility: DataSet[(Int, String, Double)] = probabilityDataSet.map(line => (line._1, line._4))
-          .groupBy(0)
-          .reduce((line1, line2) => (line1._1, line1._2))
-          .join(sumPwc).where(0).equalTo(1) {
-          (prob, pwc) => (pwc._1, prob._1, prob._2 + pwc._3)
-        }
-
-        val calculatedClasses: DataSet[(Int, String)] = possibility.groupBy(0).reduce(new CalculateReducer()).map(line => (line._1, line._2))
-
-        //TODO -> Wie "liefere" ich das Ergebnis?
+      // Splits the texts from the input DataSet into its words
+      val words: DataSet[(Int, String)] = input.flatMap {
+        pair => pair._2.split(" ").map { word => (pair._1, word)}
       }
+
+      // genreate word counts for each word with a key
+      // 1. Map: put a 1 to each key
+      // 2. Group-Reduce: group by id and word and sum the 1s
+      val wordsAndCount: DataSet[(Int, String, Int)] = words.map(line => (line._1, line._2, 1)).groupBy(0, 1).sum(2) // id -> word -> wordcount in text
+
+      //calculate the count of all words for a text identified by its key
+      val wordsInText: DataSet[(Int, Int)] = wordsAndCount.map(line => (line._1, line._3)).groupBy(0).sum(1) //id -> all words in text
+
+
+      //join probabilityDataSet and words
+      val joinedWords = wordsAndCount.join(probabilityDataSet).where(1).equalTo(1) {
+        (words, probabilityDataSet) => (words._1, probabilityDataSet._1, words._2, words._3, probabilityDataSet._3, probabilityDataSet._4, probabilityDataSet._5)
+      } // id -> classname -> Word -> wordcount in text (each word) -> log(P(w|c)) -> log(P(c)) -> log(p(w|c)) not in class
+
+      //calculate sumpwc for found words
+      // 1. Map: only needed information: id -> class -> wordcount in text (each word) *  log(p(w|c)) each word
+      // 2. Group-Reduce: sum log(p(w|c))
+      val sumPwcFoundWords: DataSet[(Int, String, Double)] = joinedWords.map(line => (line._1, line._2, line._4 * line._5))
+        .groupBy(0, 1)
+        .reduce((line1, line2) => (line1._1, line1._2, line1._3 + line2._3)) // id -> class -> sum log(p(w|c)) for all found words in class
+
+      //calculate the amount of found words for each class
+      // 1. Map: only needed information: id -> class -> wordcount in text (each word) -> log(p(w|c)) not in class
+      // 2. Group-Reduce: sum of word counts in text
+      // 3. Join with wordsInText to get the count of all words per text
+      // 4. Map: calculate sumPwcNotFound: id -> class -> (word counts in text -  wordsInText) * log(p(w|c)) not in class
+      val sumPwcNotFoundWords: DataSet[(Int, String, Double)] = joinedWords.map(line => (line._1, line._2, line._4, line._7))
+        .groupBy(0, 1)
+        .reduce((line1, line2) => (line1._1, line1._2, line1._3 + line2._3, line1._4))
+        .join(wordsInText).where(0).equalTo(0) {
+        (line1, line2) => (line1._1, line1._2, line1._3, line1._4, line2._2)
+      }.map(line => (line._1, line._2, (line._5 - line._3) * line._4)) // id -> class -> sum log(p(w|c)) for all not found words
+
+      //sum those pwc sums
+      // 1. Join sumPwcFoundWords and sumPwcNotFoundWords
+      // 2. Map: add sums from found words and sums from not found words
+      val sumPwc: DataSet[(Int, String, Double)] = sumPwcFoundWords.join(sumPwcNotFoundWords).where(0, 1).equalTo(0, 1) {
+        (found, notfound) => (found._1, found._2, found._3 + notfound._3)
+      } //id -> classname -> sum log(p(w|c))
+
+      //calculate possibility for each class
+      // 1. only needed information: class -> log(p(c))
+      // 2. Group-Reduce probabilitySet to one entry for each class
+      // 3. Join sumPwc with probability DataSet to get log(P(c))
+      // 4. Map: add sumPwc with log(P(c))
+      val possibility: DataSet[(Int, String, Double)] = probabilityDataSet.map(line => (line._1, line._4))
+        .groupBy(0)
+        .reduce((line1, line2) => (line1._1, line1._2))
+        .join(sumPwc).where(0).equalTo(1) {
+        (prob, pwc) => (pwc._1, prob._1, prob._2 + pwc._3)
+      }
+
+      //That is the return
+      possibility.groupBy(0).reduce(new CalculateReducer()).map(line => (line._1, line._2))
+
+
+
     }
-
   }
 
 
--- flink-staging/flink-ml/src/test/scala/org/apache/flink/ml/classification/NormalNaiveBayesITSuite.scala
+++ flink-staging/flink-ml/src/test/scala/org/apache/flink/ml/classification/NormalNaiveBayesITSuite.scala
@@ -26,10 +26,41 @@ import org.scalatest.{FlatSpec, Matchers}
 
 class NormalNaiveBayesITSuite extends FlatSpec with Matchers with FlinkTestBase {
 
-  val saveLocationModel = "/Users/jonathanhasenburg/Desktop/naiveB/model.csv"
+  val saveLocationModel = "/Users/jonathanhasenburg/OneDrive/datasets/bbc/runs/run1a/model.csv"
 
   behavior of "The NormalNaiveBayes implementation"
 
+  it should "train a NaiveBayesClassifier" in {
+    val env = ExecutionEnvironment.getExecutionEnvironment
+    env.setParallelism(1)
+    val nnb = NormalNaiveBayes()
+
+    val trainingDS = env.readCsvFile[(String, String)]("/Users/jonathanhasenburg/OneDrive/datasets/bbc/runs/run1a/bbcTrain.csv", "\n", "\t")
+    nnb.fit(trainingDS);
+    nnb.saveModelDataSet(saveLocationModel)
+
+    env.execute()
+  }
+
+  it should "use the trained model to predict" in {
+
+    val env = ExecutionEnvironment.getExecutionEnvironment
+    env.setParallelism(1)
+
+    val modelSet = env.readCsvFile[(String, String, Double, Double, Double)](saveLocationModel, "\n", "|")
+
+    val nnb = NormalNaiveBayes()
+    nnb.loadModelDataSet(modelSet)
+
+    val solution = nnb.predict(env.readCsvFile[(Int, String)]("/Users/jonathanhasenburg/OneDrive/datasets/bbc/runs/run1a/bbcTest.csv", "\n", "\t"))
+
+    solution.writeAsCsv("/Users/jonathanhasenburg/OneDrive/datasets/bbc/runs/run1a/bbcTestComputedCategories.csv", "\n", "\t", WriteMode.OVERWRITE)
+
+    env.execute()
+
+  }
+
+  /**
   it should "train a NaiveBayesClassifier" in {
     val env = ExecutionEnvironment.getExecutionEnvironment
     env.setParallelism(1)
@@ -66,5 +97,5 @@ class NormalNaiveBayesITSuite extends FlatSpec with Matchers with FlinkTestBase 
     env.execute()
 
   }
-
+*/
 }
